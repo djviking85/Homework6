7f7feab8c7ee4a6013f8126ac3361faad919f641
Viking <djviking@yandex.ru>
2023-01-19 16:50:20 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        {
+            for(int i = 0; i <= 10; i = i + 1){
+                System.out.println("Итерация цикла " + i);
+            }
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        {
+            for(int i = 10; i >= 0; i = i - 1){
+                System.out.println("Итерация цикла " + i);
+            }
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        {
+            //Выведите в консоль все четные числа от 0 до 17.
+            {
+                for(int i = 0; i <= 17; i = i + 2){
+                    System.out.println("Итерация цикла " + i);
+                }
+            }
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        {
+            //Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
+            {
+                for(int i = 10; i >= -10; i = i - 1){
+                    System.out.println("Итерация цикла " + i);
+                }
+            }
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        {
+            //
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        {
+            //
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        {
+            //
+        }
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        {
+            //
+        }
+    }
+    public static void task9() {
+        System.out.println("Задача 9");
+        {
+            //
+        }
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
+        {
+            //
+        }
+    }
+
 }
